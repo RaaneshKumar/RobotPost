7122524b1fd4a6b022b8ef3ba4c4cb8622a05a7b
Raanesh <raaneshkumar.k.r@metamation.com>
2025-02-04 16:23:25 +0530

    Implemented Previous position check.

--- RobotPost/Program.cs
+++ RobotPost/Program.cs
@@ -1,6 +1,6 @@
 ﻿using static System.Console;
 
-if (args.Length == 0) { WriteLine ("no input!"); ReadKey (); return; }
+if (args.Length == 0) { WriteLine ("No input!"); ReadKey (); return; }
 var rpp = new RobotPost (args[0]);
 rpp.GenOutputFiles ();
 WriteLine ("File generated successfully.");
