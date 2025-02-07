267966b7c5ade7725e4835c903f944854f8076da
Raanesh <raaneshkumar.k.r@metamation.com>
2025-02-07 16:55:02 +0530

    Added valid directory check.

--- RobotPost/Program.cs
+++ RobotPost/Program.cs
@@ -3,19 +3,19 @@
 if (args.Length == 0) { WriteLine ("No input!"); ReadKey (); return; }
 var rpp = new RobotPost (args[0]);
 
-//Console.Write ("Enter the hard code files directory: ");
-//string hcPath = Console.ReadLine ();
-//if (!Directory.Exists (hcPath)) { Console.WriteLine ("Invalid directory"); return;}
-//if (!File.Exists (hcPath + "/BendLS_NoRegripHC.txt") 
-//     || !File.Exists (hcPath + "/BendLS_RegripHC.txt")
-//     || !File.Exists (hcPath + "/BendSub_HC(PG).txt")
-//     || !File.Exists (hcPath + "/BendSub_HC(VG).txt")
-//     || !File.Exists (hcPath + "/Header.txt")
-//     || !File.Exists (hcPath + "/MainLS_HC(PG).txt")
-//     || !File.Exists (hcPath + "/MainLS_HC(VG).txt")) {
-//   Console.WriteLine ("One or many required hard code files does not exist.");
-//   return;
-//}
+Console.Write ("Enter the hard code files directory: ");
+string hcPath = Console.ReadLine ();
+if (!Directory.Exists (hcPath)) { Console.WriteLine ("Invalid directory"); return; }
+if (!File.Exists (hcPath + "/BendLS_NoRegripHC.txt")
+     || !File.Exists (hcPath + "/BendLS_RegripHC.txt")
+     || !File.Exists (hcPath + "/BendSub_HC(PG).txt")
+     || !File.Exists (hcPath + "/BendSub_HC(VG).txt")
+     || !File.Exists (hcPath + "/Header.txt")
+     || !File.Exists (hcPath + "/MainLS_HC(PG).txt")
+     || !File.Exists (hcPath + "/MainLS_HC(VG).txt")) {
+   Console.WriteLine ("One or many required hard code files does not exist.");
+   return;
+}
 
 rpp.GenOutputFiles ();
 WriteLine ("File generated successfully.");
